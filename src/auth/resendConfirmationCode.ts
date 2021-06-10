167b08aaa1da3b4620db3280a3b46cbf4556553a
pxai <pello.altadill@gmail.com>
2021-06-10 08:59:00 +0200

    Added SignIn

---
+++ src/auth/resendConfirmationCode.ts
@@ -0,0 +1,10 @@
+import { Auth } from 'aws-amplify';
+
+async function resendConfirmationCode(username: string) {
+    try {
+        await Auth.resendSignUp(username);
+        console.log('code resent successfully');
+    } catch (err) {
+        console.log('error resending code: ', err);
+    }
+}
---
+++ src/auth/signIn.ts
@@ -0,0 +1,9 @@
+import { Auth } from 'aws-amplify';
+
+async function signIn(username: string, password: string) {
+    try {
+        const user = await Auth.signIn(username, password);
+    } catch (error) {
+        console.log('error signing in', error);
+    }
+}
